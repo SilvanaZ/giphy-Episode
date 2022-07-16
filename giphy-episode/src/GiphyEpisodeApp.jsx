3ed3a0eae42cae996060aff5275089f9fa022293
Silvana Zalazar <90283693+SilvanaZ@users.noreply.github.com>
2022-07-16 00:56:20 -0300

    Update GiphyEpisodeApp.jsx

--- giphy-episode/src/GiphyEpisodeApp.jsx
+++ giphy-episode/src/GiphyEpisodeApp.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const GiphyEpisodeApp = () => {
+
+    const [categories, setCategories] = useState(['One Piece', 'One Punchman', 'Nanatzu no Taizai', 'Kimetsu no Yaiba']);
+
+    console.log(categories)
     return (
         <>
             {/* titulo */}
             <h1> GiphyEpisodeApp</h1>
+
             {/* Input */}
 
             {/* Listado de Gif */}
+            <ol>
+                {categories.map(category => {
+                    return <li key={category}>{category}</li>
+                })}
+            </ol>
+
             {/* Gif Item */}
         </>
     )
